fbcc73b1f6bd96c57e7d811548fa76f3ec59f980
Licotch <licotch@licot.ch>
2018-08-07 20:56:09 +0900

    solved B

--- AtCoder-Beginners-Contest-104/B.lisp
+++ AtCoder-Beginners-Contest-104/B.lisp
@@ -1,5 +1,13 @@
 ;;;; B - AcCepted
 ;;;; https://beta.atcoder.jp/contests/abc104/tasks/abc104_b
 
-;;;;   Time:
-;;;; Memory:
+;;;;   Time: 16ms
+;;;; Memory: 3936KB
+
+(let ((s (read-line)))
+  (princ (if (and (char= (char s 0) #\A)
+                  (= 1 (count #\C s :start 2 :end (1- (length s)) :test #'char=))
+                  (not (some #'upper-case-p
+                             (remove #\C (subseq s 1) :test #'char= :count 1))))
+             "AC"
+             "WA")))
