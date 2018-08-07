6b3a3f00a6d4be8fd0356b5fd7e749aa5cb7e97f
Licotch <licotch@licot.ch>
2018-08-07 20:24:10 +0900

    solved A

--- AtCoder-Beginners-Contest-104/A.lisp
+++ AtCoder-Beginners-Contest-104/A.lisp
@@ -1,5 +1,11 @@
 ;;;; A - Rated for Me
 ;;;; https://beta.atcoder.jp/contests/abc104/tasks/abc104_a
 
-;;;;   Time:
-;;;; Memory:
+;;;;   Time: 8ms
+;;;; Memory: 3176KB
+
+(let ((rate (read)))
+  (cond
+    ((< rate 1200) (princ "ABC"))
+    ((< rate 2800) (princ "ARC"))
+    (t (princ "AGC"))))
