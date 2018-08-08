c5e9f98fcdea2396e536af5c6d8f351a588c91c8
Licotch <licotch@licot.ch>
2018-08-08 17:28:49 +0900

    solved A

---
+++ AtCoder-Beginners-Contest-100/A.lisp
@@ -0,0 +1,7 @@
+;;;; A - Happy Birthday!
+;;;; https://beta.atcoder.jp/contests/abc100/tasks/abc100_a
+
+(princ (if (and (<= (read) 8)
+                (<= (read) 8))
+           "Yay!"
+           ":("))
